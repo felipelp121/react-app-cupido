150cc7bd304a9ca68b42358b9438ff1070f9445e
felipelp121 <felopesserra@id.uff.br>
2022-03-25 17:48:20 -0300

    refatorando

--- src/services/sendEmail.js
+++ src/services/sendEmail.js
@@ -1,13 +1,22 @@
+import { Auth } from "aws-amplify";
 export async function sendEmail(email, subject, text) {
+  const user = await Auth.currentUserInfo();
+  // console.log("USER:", user.username);
   const response = await fetch(
-    "https://613hrltlv6.execute-api.us-east-1.amazonaws.com/dev/send",
+    "https://z7x57qrr32.execute-api.us-east-1.amazonaws.com/dev/send",
     {
       method: "POST",
       headers: {
         Accept: "application/json",
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ subject: subject, email: email, text: text }),
+      body: JSON.stringify({
+        subject: subject,
+        email: email,
+        text: text,
+        userEmail: user.attributes.email,
+        userName: user.attributes.username,
+      }),
     }
   ).then((response) => {
     if (response.ok) {
