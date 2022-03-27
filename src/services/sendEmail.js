5cd09398aa79c3e749c14fd746885f8741c0f93c
felipelp121 <felopesserra@id.uff.br>
2022-03-26 21:53:33 -0300

    adicionando outro fetch

--- src/services/sendEmail.js
+++ src/services/sendEmail.js
@@ -1,7 +1,7 @@
 import { Auth } from "aws-amplify";
 export async function sendEmail(email, subject, text) {
   const user = await Auth.currentUserInfo();
-  // console.log("USER:", user.username);
+  console.log("USER:", user);
   const response = await fetch(
     "https://z7x57qrr32.execute-api.us-east-1.amazonaws.com/dev/send",
     {
@@ -15,15 +15,48 @@ export async function sendEmail(email, subject, text) {
         email: email,
         text: text,
         userEmail: user.attributes.email,
-        userName: user.attributes.username,
+        userName: user.username,
+      }),
+    }
+  );
+  //   .then((responseFetch) => {
+  //   if (responseFetch.ok) {
+  //     console.log(responseFetch.body);
+  //     return "sucessed";
+  //   }
+  //   console.log(responseFetch);
+  //   return "failed";
+  // });
+  const responseJson = await response.json();
+
+  await fetch(
+    "https://qqqlarfevc.execute-api.us-east-1.amazonaws.com/dev/received",
+    {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        subject: subject,
+        email: email,
+        text: text,
+        userEmail: user.attributes.email,
+        userName: user.username,
+        messageId: responseJson,
       }),
     }
   ).then((response) => {
     if (response.ok) {
-      return "sucessed";
+      console.log("FETCH OK");
     }
-    return "failed";
+    if (!response.ok) {
+      console.log("FETCH FAILED");
+    }
   });
-
-  return response;
+  if (responseJson) {
+    return "sucessed";
+  } else {
+    return "failed";
+  }
 }
