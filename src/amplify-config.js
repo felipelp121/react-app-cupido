e839939b75661a29e65d2035963a6029447083cf
felipelp121 <felopesserra@id.uff.br>
2022-04-08 20:03:13 -0300

    Finalizando projeto

--- src/amplify-config.js
+++ src/amplify-config.js
@@ -1,7 +1,7 @@
 const amplifyConfig = {
   aws_cognito_region: "us-east-1",
-  aws_user_pools_id: process.env.POOLS_ID,
-  aws_user_pools_web_client_id: process.env.POOLS_CLIENT_ID,
+  aws_user_pools_id: "us-east-1_X5gYngjUE",
+  aws_user_pools_web_client_id: "6garhb9ga8b522b6u7qns3gok2",
 };
 
 export default amplifyConfig;
